fc0733643e8ae026a070e5fc583abe9da2ee5478
Vinay <47492067+the-go-getter@users.noreply.github.com>
2025-09-21 09:56:16 -0600

    Delete leetcode-python/solution_5.py

--- leetcode-python/solution_5.py
+++
@@ -1,7 +0,0 @@
-﻿# Simple seeded file for timeline commits
-def add(a, b):
-    return a + b
-# TODO: replace with real LeetCode solution
-# practice tweak 2025-04-04
-# practice tweak 2025-06-24
-# practice tweak 2025-08-24
